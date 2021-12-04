3439560e1de331205d6bf1f7636db838d6a51606
Johnny Mendes <menjo95@gmail.com>
2021-12-04 19:35:11 +0100

    added main and tests

---
+++ src/main/java/com/temenos/t24/account.java
@@ -0,0 +1,23 @@
+package com.temenos.t24;
+
+public class account {
+
+
+
+  public static void main(String[] args) {
+
+    System.out.println("Hello guys");
+
+
+  }
+
+  public int addition() {
+
+    int num1 = 1;
+    int num2 = 2;
+
+    int result = num1 + num2;
+
+    return result;
+  }
+}
---
+++ src/test/java/test.java
@@ -0,0 +1,15 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import com.temenos.t24.*;
+
+import org.junit.jupiter.api.Test;
+
+class JUnit5ExampleTest {
+
+  @Test
+  void justAnExample() {
+
+    account acc = new  account();
+    int result = acc.addition();
+    assertEquals(3,result);
+  }
+}
